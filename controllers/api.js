2f6cd1fd2b80fb2eae9b8d746bab43201d421d23
Tay West <tayanne.west@gmail.com>
2021-12-17 09:38:20 -0500

    create api controller and stub up first functions

---
+++ controllers/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+
+// just stubbing up functions atm
+function searchStores(req, res) {
+  console.log("haahaahaa")
+
+  
+}
+
+function searchRestaurants(req, res) {
+  console.log("heeheehee")
+
+  
+}
+
+function searchBars(req, res) {
+  console.log("hoohoohoo")
+
+}
+
+
+export {
+  searchStores,
+  searchRestaurants,
+  searchBars,
+}
