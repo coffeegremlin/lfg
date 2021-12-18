119599567a2eaef045e7ae1e855718a62b30e5ce
SullyDurgin <katsdurgin@gmail.com>
2021-12-18 17:00:33 -0500

    myMessage code to render mymessage on page

--- src/components/Chat/MyMessage.jsx
+++ src/components/Chat/MyMessage.jsx
@@ -1,9 +1,21 @@
 //This file is the chat bubble that we send
 
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+  //tell between image or text messages
+  if(message?.attachments?.length > 0 ) { 
+      return (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ float: 'right' }}
+
+        />
+      )
+  }
   return (
-    <div>
-      MyMessage
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: 'indigo' }}>
+      {message.text}
     </div>
 
   )
