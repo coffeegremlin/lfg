23196a427462bf423961c5cd490d811fb872f359
SullyDurgin <katsdurgin@gmail.com>
2021-12-18 13:29:08 -0500

    fix chat syle drop down and log in box

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -4,12 +4,14 @@ import './Chat.css';
 
 const Chat = () => {
   return (
+		<div className='chat-page'>
 		<ChatEngine
 			height='100vh'
 			projectID='2b543fe2-2a38-4552-8b53-6ae71ea17344'
 			userName='KateDurgin'
 			userSecret='123123'
 		/>
+		</div>
 	)
 }
 
