7f3820bfc675556dba8c01f114015b3e32f6b100
SullyDurgin <katsdurgin@gmail.com>
2021-12-18 19:02:27 -0500

    filling out form info

--- src/components/Chat/LoginForm.jsx
+++ src/components/Chat/LoginForm.jsx
@@ -1,3 +1,30 @@
 import { useState } from 'react';
 import axios from 'axios';
  
+const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start chatting</span>
+            </button>
+          </div>
+        </form>
+        <h1>{error}</h1>
+      </div>
+    </div>
+
+  );
+};
