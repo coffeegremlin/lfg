1ffb19e9c994258c32d9d74511c64c7986ece93f
ScottR <astroplexity@gmail.com>
2021-12-20 20:59:52 -0500

    event pages added

---
+++ src/pages/Event/Event.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import EventForm from '../../components/EventForm/EventForm'
+import styles from './Event.css'
+
+const Events = props => {
+  const [events, setEvents] = useState([''])
+
+  const updateEvent = evnt => {
+    setEvents(evnt)
+  }
+
+  return (
+    <main>
+      <h1>Create an Event</h1>
+      <p>{events}</p>
+      <EventForm {...props} updateEvent={updateEvent} />
+    </main>
+
+  )
+}
+
+export default Events
