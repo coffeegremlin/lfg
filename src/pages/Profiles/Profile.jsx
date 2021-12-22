107fc59dc6d532fe334020afcb89ea7046ba4618
SullyDurgin <katsdurgin@gmail.com>
2021-12-21 19:04:38 -0500

    style profile

--- src/pages/Profiles/Profile.jsx
+++ src/pages/Profiles/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import './Profile.css'
 
 const Profile = ({ profile }) => {
 
@@ -8,11 +9,11 @@ const Profile = ({ profile }) => {
 
   return (
     <main>
-      <h1>{profileInfo.name}'s profile</h1>
-      <img src={profileInfo.avatar} alt={`${profileInfo.name}'s avatar`} />
+      <h1 className='profile-header'>{profileInfo.name}'s profile</h1>
+      <img className='profile-avatar' src={profileInfo.avatar} alt={`${profileInfo.name}'s avatar`} />
       <div className='profile-info'>
-        <h3>Bio:
-          <p>{profileInfo.bio}</p>
+        <h3 className='bio-header'> Bio:
+          <p className='bio-body'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.{profileInfo.bio}</p>
         </h3>
       </div>
       {/* add in a place for user photo */}
